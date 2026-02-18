28fec9bcaede83cf7c7ec67c23ef0ea4ba7b2261
steven <stevocriss9@gmail.com>
2026-02-18 05:58:32 -0800

    feature-login

---
+++ src/Views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+    <h1>This Page is Not Found</h1>
+   <h3>Go back to Home page</h3>
+    <router-link to="/" >Go back to home</router-link>
+    </div>
+    <router-view/>
+</template>
+<script>
+
+export default{
+    name:"NotFound"
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+createApp(App).use(router).mount('#app')
+
+import "./assets/style.css"
